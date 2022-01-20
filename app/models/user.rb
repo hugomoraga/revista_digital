a73ec06b5c778c8453b88a9c2d6bc9c42a77e02e
Hugo M <hg.moraga@gmail.com>
2022-01-20 15:30:41 -0300

    Add Roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+  enum role: [:Periodista, :Editor, :Administrador]
+  after_initialize :set_default_role, :if => :new_record?
+  def set_default_role
+      self.role ||= :user
+  end
 end
