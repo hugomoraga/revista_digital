25c516ec71ecdd74d4abbd15ad14793c5901dae2
Hugo Moraga <hugo.moraga@tchile.com>
2022-01-26 16:42:10 -0300

    deployment devserver desarollo2.tchile.com

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,4 +64,5 @@ Rails.application.configure do
 
   # Devise Setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.hosts << "revistadigital.tchile.com"
 end
